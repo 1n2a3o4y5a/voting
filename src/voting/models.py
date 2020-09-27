f2f5160fcb734404cdc22524ad0318422ab5e53f
岸直哉 <user.id.125125@gmail.com>
2020-09-27 17:18:57 +0900

    投票日時追加

--- src/voting/models.py
+++ src/voting/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Comic(models.Model):
     title = models.CharField(max_length=100)
@@ -20,3 +21,4 @@ class Character(models.Model):
 class voting(models.Model):
     name = models.ForeignKey(Character, on_delete=models.CASCADE)
     point = models.IntegerField()
+    created_at = models.DateTimeField(verbose_name='投票日時', default=timezone.now, editable=False)
