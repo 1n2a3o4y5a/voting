0c6e3edd0451ea9c91bba8cf7608e5f76d8473ef
岸直哉 <user.id.125125@gmail.com>
2020-10-06 08:05:31 +0900

    ランキング機能作成

--- src/voting/views.py
+++ src/voting/views.py
@@ -9,12 +9,12 @@ class VotingList(ListView):
 
 class VotingDetail(DetailView):
     template_name = 'ranking.html'
-    model = Voting
-    queryset = Voting.objects.all() #.filter().order_by('-point')
-    context_object_name = "items"
-
-    # def get_queryset(self):
-    #     # qs = Voting.objects.filter(name_id__comic_id__id=1).all().order_by('-point')
-    #     return Voting.objects.filter(name_id__comic_id__id=1).order_by('-point')
+    model = Comic
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["voting"] = Voting.objects.filter(name_id__comic_id__id=self.kwargs['pk']).order_by('-point')
+        # do something
+        return context
 
 
