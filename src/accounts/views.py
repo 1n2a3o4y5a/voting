4e72fb819d5b86383549b8d5bd392a949525fa87
岸直哉 <user.id.125125@gmail.com>
2020-10-08 08:01:47 +0900

    signupテンプレート

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.conf.urls.static import static
+
  
  
 class SignUpView(CreateView):
--- src/app/urls.py
+++ src/app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import include, path
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('voting.urls')),
-    path('accounts/',include('accounts.urls'))
-]
+    path('',include('accounts.urls'))
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
