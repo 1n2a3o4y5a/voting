eab483c9cafd778e194dde05090881020ff21afa
岸直哉 <user.id.125125@gmail.com>
2020-10-08 08:44:05 +0900

    signup途中

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -2,10 +2,26 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.conf.urls.static import static
+from . forms import UserCreateForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
 
- 
- 
+#アカウント作成
 class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'accounts/signup.html'
+    def post(self, request, *args, **kwargs):
+        form = UserCreateForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            #フォームから'username'を読み取る
+            username = form.cleaned_data.get('username')
+            #フォームから'password1'を読み取る
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+        return render(request, 'accounts/signup.html', {'form': form,})
+
+    def get(self, request, *args, **kwargs):
+        form = UserCreateForm(request.POST)
+        return  render(request, 'accounts/signup.html', {'form': form,})
