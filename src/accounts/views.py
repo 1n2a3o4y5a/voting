3ac4fe48b3f13b63fb03b122fdd3a0c89074b7d8
岸直哉 <user.id.125125@gmail.com>
2020-10-11 22:44:34 +0900

    途中

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,7 +1,23 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from .forms import SignupForm
+from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
+from django.contrib.auth import login
+
+
+
+
 
 class LogInView(TemplateView):
     template_name = "./accounts/login.html"
 
-class SignUpView(TemplateView):
+class SignUpView(CreateView):
+    form_class = SignupForm
     template_name = "./accounts/signup.html"
+    success_url = reverse_lazy('list')
+
+    def form_valid(self, form):
+        user = form.save() 
+        login(self.request, user)
+        self.object = user 
+        return HttpResponseRedirect(self.get_success_url())
